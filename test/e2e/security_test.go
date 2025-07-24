6bcb85cc00b19ae8ca1ed1a724d40bac3e8451c7
Alexander Knish <alexandr.knish@gmail.com>
2025-07-24 18:37:28 +0300

    Fix unused imports and add go vet to pre-commit
    - Remove unused k8s.io/apimachinery/pkg/api/errors import
    - Remove unused sigs.k8s.io/controller-runtime/pkg/client import
    - Add go vet to pre-commit hooks for early detection of issues
    - Fix go vet hook to use system language and correct path

--- test/e2e/security_test.go
+++ test/e2e/security_test.go
@@ -9,11 +9,9 @@ import (
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/intstr"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 var _ = Describe("Pod Security Standards", func() {
